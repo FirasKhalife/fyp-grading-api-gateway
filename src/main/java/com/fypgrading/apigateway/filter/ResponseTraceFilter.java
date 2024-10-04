16ec1c41f557455d939bcc35a008566b886f1930
Firas Khalife <khalifefiras29@gmail.com>
2024-10-05 01:57:43 +0300

    cloud-course optimize code, fix and complete aspect logging, refactor mapping and DTOs

--- src/main/java/com/fypgrading/apigateway/filter/ResponseTraceFilter.java
+++ src/main/java/com/fypgrading/apigateway/filter/ResponseTraceFilter.java
@@ -1,20 +1,18 @@
 package com.fypgrading.apigateway.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 public class ResponseTraceFilter implements GlobalFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(ResponseTraceFilter.class);
-
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
-        logger.debug("Adding Response Time to response");
+        log.debug("Adding Response Time to response");
         exchange.getResponse().getHeaders().add("X-Response-Time", String.valueOf(System.currentTimeMillis()));
         return chain.filter(exchange);
     }
